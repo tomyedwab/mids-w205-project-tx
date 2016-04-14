d4d7c106613058aba5d2b9c71d7df8631442ad8a
Adam Spitzig <adamspitzig@gmail.com>
2016-04-14 16:04:35 -0700

    Add script to process bikeshare data and copy into HDFS

---
+++ etl/bikeshare/bikeshare.py
@@ -0,0 +1,45 @@
+import subprocess
+
+# Sources for bikeshare data
+# year_1_data = 'https://s3.amazonaws.com/babs-open-data/babs_open_data_year_1.zip'
+# year_2_data =  'https://s3.amazonaws.com/babs-open-data/babs_open_data_year_2.zip'
+
+class BikeshareImport(object):
+
+    def __init__(self, DATA_DIR):
+        self.DATA_DIR = DATA_DIR
+
+    def do_import(self):  # TODO: working locally, test on Docker machine
+
+        # print "Downloading and unzipping bikeshare data..." # TODO: not functional currently - all individual csv files must already be in /data/bikeshare_raw before script execution
+        # subprocess.call('wget %s -O temp.zip; unzip temp.zip -d %s/bikeshare_raw; rm temp.zip' % (year_1_data, self.DATA_DIR))
+        # subprocess.call('wget %s -O temp.zip; unzip temp.zip -d %s/bikeshare_raw; rm temp.zip' % (year_2_data, self.DATA_DIR))
+
+        print "Processing bikeshare data and copying into HDFS..."
+
+        # process station data  (works locally)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201402_station_data.csv > %s/bikeshare_raw/station_data_1.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201408_station_data.csv > %s/bikeshare_raw/station_data_2.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201508_station_data.csv > %s/bikeshare_raw/station_data_3.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('cat %s/bikeshare_raw/station_data_1.csv %s/bikeshare_raw/station_data_2.csv %s/bikeshare_raw/station_data_3.csv > %s/bikeshare_raw/station_data.csv' % (self.DATA_DIR, self.DATA_DIR, self.DATA_DIR, self.DATA_DIR), shell=True)
+
+        # process status data   (works locally)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201402_status_data.csv > %s/bikeshare_raw/status_data_1.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201408_status_data.csv > %s/bikeshare_raw/status_data_2.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('tail -n +2 %s/bikeshare_raw/201508_status_data.csv > %s/bikeshare_raw/status_data_3.csv' % (self.DATA_DIR, self.DATA_DIR), shell=True)
+        subprocess.call('cat %s/bikeshare_raw/status_data_1.csv %s/bikeshare_raw/status_data_2.csv %s/bikeshare_raw/status_data_3.csv > %s/bikeshare_raw/status_data.csv' % (self.DATA_DIR, self.DATA_DIR, self.DATA_DIR, self.DATA_DIR), shell=True)
+
+        # create directories in HDFS (TODO test on Docker machine)
+        subprocess.call('/vagrant/hadoop/hadoop-hdfs.sh', shell=True)
+        subprocess.call('dfs -mkdir hdfs://hadoop:9000/station_data', shell=True)
+        subprocess.call('dfs -mkdir hdfs://hadoop:9000/status_data', shell=True)
+
+        # copy processed station and status data to directories in HDFS (TODO test on Docker machine)
+        subprocess.call('dfs -put %s/bikeshare_raw/station_data.csv hdfs://hadoop:9000/station_data' % self.DATA_DIR, shell=True)
+        subprocess.call('dfs -put %s/bikeshare_raw/status_data.csv hdfs://hadoop:9000/status_data' % self.DATA_DIR], shell=True)
+
+if __name__ == '__main__':
+
+    DATA_DIR = '../../data' # testing locally
+    importer = BikeshareImport(DATA_DIR)
+    importer.do_import()
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python
 
 import datetime
-import subprocess
 
 import secrets
 from etl.weather.forecastio import ForecastIOImport
+from etl.bikeshare.bikeshare import BikeshareImport
 
 START_DATE = datetime.datetime(2013, 8, 29)
 END_DATE = datetime.datetime(2015, 8, 31)
 MAX_REQUESTS = 1
 DATA_DIR = "/vagrant/data"
 
-importer = ForecastIOImport(secrets, DATA_DIR)
-importer.do_import(START_DATE, END_DATE, MAX_REQUESTS)
-#importer.copy_to_hdfs()
+importer_weather = ForecastIOImport(secrets, DATA_DIR)
+importer_weather.do_import(START_DATE, END_DATE, MAX_REQUESTS)
+#importer_weather.copy_to_hdfs()
 
-# Copy the raw bikeshare data to HDFS as well
-print "Copying bikeshare data to HDFS..."
-subprocess.call(
-    "/vagrant/hadoop/hadoop-hdfs.sh "
-    "dfs -put %s/bikeshare_raw hdfs://hadoop:9000/" % DATA_DIR,
-    shell=True
-)
+importer_bikeshare = BikeshareImport(DATA_DIR)
+importer_bikeshare.do_import()
+
 print "Done."
