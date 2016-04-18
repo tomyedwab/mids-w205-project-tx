bc1aa4f586f4adbe5e31223c949e6abf78eb3af1
Tom W. Yedwab <tom@khanacademy.org>
2016-04-17 19:32:33 -0700

    Added Spark parsing for BikeShare data.

---
+++ etl/bikeshare/spark-bikeshare-transform.py
@@ -0,0 +1,61 @@
+#!/etc/spark/bin/pyspark
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+
+sc = SparkContext("local", "Bike Share ETL", pyFiles=[])
+sqlContext = SQLContext(sc)
+
+station_csv = (sc.textFile("hdfs://hadoop:9000/station_data")
+    .map(lambda line: line.split(","))
+    .filter(lambda line: len(line)>1)
+    .map(lambda line: (
+        int(line[0]), line[1], float(line[2]), float(line[3]),
+        int(line[4]), line[5], line[6]
+    )))
+
+schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("latitude", FloatType(), True),
+    StructField("longitude", FloatType(), True),
+    StructField("dockCount", IntegerType(), True),
+    StructField("city", StringType(), True),
+    StructField("installed", StringType(), True),
+])
+
+# Apply the schema to the RDD.
+station_df = sqlContext.createDataFrame(station_csv, schema)
+print "#######"
+print station_df.printSchema()
+print station_df.show()
+print "#######"
+
+status_csv = (sc.textFile("hdfs://hadoop:9000/status_data")
+    .map(lambda line: line.split(","))
+    .filter(lambda line: len(line)>1)
+    .map(lambda line: (
+        int(line[0].replace("\"", "")),
+        int(line[1].replace("\"", "")),
+        int(line[2].replace("\"", "")),
+        line[3][1:11],
+        line[3][12:17],
+    )))
+
+schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("bikesAvailable", IntegerType(), True),
+    StructField("docksAvailable", IntegerType(), True),
+    StructField("date", StringType(), True),
+    StructField("time", StringType(), True),
+])
+
+# Apply the schema to the RDD.
+status_df = sqlContext.createDataFrame(status_csv, schema)
+print "#######"
+print status_df.printSchema()
+print status_df.show()
+print status_df.filter("bikesAvailable = 0").show()
+print "#######"
